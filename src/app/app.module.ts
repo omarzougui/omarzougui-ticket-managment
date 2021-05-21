af8632beffdac75ee24e91b11575b8043a107767
Omar Marzougui <omarzougui.23@gmail.com>
2021-05-21 20:09:25 +0200

    chore: install needed libs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BackendService } from './backend.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule],
+    imports: [BrowserModule, BrowserAnimationsModule],
     providers: [BackendService],
     bootstrap: [AppComponent]
 })
