be956d4b9ce37f936de32a1473af6d5911c460d8
Omar Marzougui <omarzougui.23@gmail.com>
2021-05-24 09:48:26 +0200

    feat: creat ticket list

--- src/app/features/ticket/ticket-list/ticket-list.component.ts
+++ src/app/features/ticket/ticket-list/ticket-list.component.ts
@@ -1,11 +1,76 @@
 import { Component, OnInit } from '@angular/core';
+import { Ticket } from '@core/models/interfaces/ticket.interface';
+import { BackendService } from '@core/services/backend.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
+import { RootModules } from '@core/root-modules.enum';
+import { combineLatest } from 'rxjs';
+import { map } from 'rxjs/operators';
+import * as _ from 'lodash';
 
+export type TicketWithoutAuthorName = Omit<FormattedTicket, 'author'>;
+
+export interface FormattedTicket extends Ticket {
+    completedString: 'Done' | 'In progress',
+    author: string,
+}
 
 @Component({
     selector: 'app-ticket-list',
     templateUrl: './ticket-list.component.html',
     styleUrls: ['./ticket-list.component.css']
 })
-export class TicketListComponent {
+export class TicketListComponent implements OnInit {
+
+    ticketDataSource: MatTableDataSource<FormattedTicket>;
+    tickets: FormattedTicket[];
+    displayedColumns: (keyof FormattedTicket)[] = ['description', 'completedString', 'author'];
+
+    constructor(
+        private backendService: BackendService,
+        private router: Router,
+    ) {
+    }
+
+    ngOnInit() {
+        this.initTicketList();
+    }
+
+
+    static formatTicketState(ticket: Ticket): TicketWithoutAuthorName {
+        return { ...ticket, completedString: ticket.completed ? 'Done' : 'In progress' }
+    }
+
+    initTicketList(): void {
+        combineLatest([
+            this.backendService.tickets().pipe(map(this.formatTicketsState)),
+            this.backendService.users(),
+        ]).subscribe(([tickets, users]) => {
+            const formattedTickets = tickets.map((ticket) => {
+                return { ...ticket, author: _.find(users, (user) => user.id === ticket.assigneeId).name }
+            })
+            this.tickets = formattedTickets;
+            this.ticketDataSource = new MatTableDataSource(formattedTickets);
+        })
+    }
+
+    applyFilter(event: Event) {
+        const filterValue = (event.target as HTMLInputElement).value;
+        this.ticketDataSource.filter = filterValue.trim().toLowerCase();
+    }
+
+    formatTicketsState(tickets: Ticket[]): TicketWithoutAuthorName[] {
+        return tickets.map((ticket) => {
+            return TicketListComponent.formatTicketState(ticket)
+        })
+    }
+
+    showTicketDetails(ticket: FormattedTicket) {
+        this.router.navigate([RootModules.TICKET, ticket.id]);
+    }
+
+
+
+
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
 
 
 @NgModule({
@@ -9,13 +15,25 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     imports: [
         CommonModule,
         HttpClientModule,
+        MatTableModule,
+        MatFormFieldModule,
+        MatInputModule,
+        FormsModule,
+        MatProgressSpinnerModule,
+        MatButtonModule,
         ReactiveFormsModule,
-
+        MatSelectModule
     ],
     exports: [
         HttpClientModule,
+        MatTableModule,
+        MatFormFieldModule,
         FormsModule,
-
+        MatInputModule,
+        MatProgressSpinnerModule,
+        MatButtonModule,
+        ReactiveFormsModule,
+        MatSelectModule
     ]
 })
 export class SharedModule {
